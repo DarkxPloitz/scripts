a62481382728a5d4c9bf1f1db4aa8387e0b7bbf6
DarkxPloitz <68434490+DarkxPloitz@users.noreply.github.com>
2020-07-17 16:06:13 +0100

    Update detect.lua

--- detect.lua
+++ detect.lua
@@ -1,1 +1,5 @@
-wa
+if game.PlaceId == 1962086868 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkxPloitz/scripts/master/towerofhell.lua"))();
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkxPloitz/scripts/master/notsupported.lua"))();
+end
